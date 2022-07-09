b3f63188f8d4f4a1f561803ee772915676390d20
Chanwoong Kim <me@chanwoong.kim>
2022-07-10 04:34:12 +0900

    Set logging basicConfig

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__main__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__main__.py
@@ -1,11 +1,18 @@
+import logging
+import sys
+
 import typer
 
 app = typer.Typer()
+logging.basicConfig(stream=sys.stderr, level=logging.INFO, format="[%(asctime)s] %(levelname)s: %(name)s: %(message)s")
+
+
+logger = logging.getLogger(__name__)
 
 
 @app.command()
 def run():
-    print("Hello, World!")
+    logger.info("Hello, World!")
 
 
 if __name__ == "__main__":
